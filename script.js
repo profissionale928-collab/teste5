9b27bd08925188a67f0be0c1cbd8448d20d4a2fd
profissionale928-collab <profissionale928@gmail.com>
2025-11-03 21:36:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Configurações da API
 const API_BASE_URL = 'https://api.cnpja.com/office';
-const API_KEY = 'ce885e22-e93a-445f-8a80-7573b008af55-669d5a8f-c063-49ea-be84-f6f10f6fdd09';
+const API_KEY = '21ffa393-e330-4328-8bc1-f2e4d2a346a8-aa1d4885-51c2-49e5-84d5-2a877c634a99';
 
 // Elementos do DOM
 const searchForm = document.getElementById('searchForm' );
